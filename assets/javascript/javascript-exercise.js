f714481ea8d245fc415e5c9a075458d37821454a
Luke Keltner <lkeltner87@gmail.com>
2017-09-09 13:08:07 -0400

    Ex and Calories can talk to each other

--- assets/javascript/javascript-exercise.js
+++ assets/javascript/javascript-exercise.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  var currentWorkout = "";
+  var currentCaloriesPerRep = 0;
+  var currentSet = "";
+
   var populateTodayExercise = function() {
     for (var x in phase1.day1) {
         var exerciseDiv = $("<button>");
@@ -13,15 +17,19 @@ $(document).ready(function() {
   populateTodayExercise();
 
   $("#exercises").on("click", function getExerciseInfo(event) {
+
+    currentWorkout = event.target.id
+
     if (event.target.className === "btn btn-primary") {
       $("#exercise-progress").empty();
       var selectedExercise = event.target.id;
             
       var numSets = phase1.day1[selectedExercise].sets;
       var numReps = phase1.day1[selectedExercise].reps;
+
       for (var i = 0; i < numSets; i++) {
         $("#exercise-progress").append("Set "+(i+1)+" ");
-        $("#exercise-progress").append("Reps: <span id='rep-counter-"+(i+1)+"'></span><br>");
+        $("#exercise-progress").append("Reps: <span id='rep-counter-"+(i+1)+"'>");
         $("#exercise-progress").append("<input type='range' min='0' max='"+numReps+"' value='0' class='slider' id='"+(i+1)+"'>");
         $("#exercise-progress").append("<label class='checkbox-inline'><input type='checkbox' value='' class='checkbox' id='checkbox-"+(i+1)+"'>Completed?</label><br><br>");     
       };
@@ -33,11 +41,34 @@ $(document).ready(function() {
   $(document).on("input", ".slider", function(event) {
     var repCounterID = event.target.id;
     $("#rep-counter-"+repCounterID).html($(this).val());
+    $("#rep-counter-"+repCounterID).data("reps", $(this).val())
   });
 
   $(document).on("click", ".checkbox", function(event) {
     var checkboxID = event.target.id;
-    console.log(checkboxID);
+    var splitCheckBoxID = checkboxID.split('')
+    var repID = splitCheckBoxID[splitCheckBoxID.length - 1]
+    currentSet = repID;
+    currentCaloriesPerRep = phase1.day1[currentWorkout].calories;
+    var amountOfReps = $("#rep-counter-"+repID).data("reps")
+    console.log("workout: "+currentWorkout+", set: "+currentSet+", reps: "+amountOfReps+" Cals per Rep: "+currentCaloriesPerRep)
+
+    var newCalories = 0;
+
+    database.ref("users").once('value', function(snap)
+    {
+      console.log(snap.val()[userID])
+      console.log(snap.val()[userID].calories)
+      console.log(currentCaloriesPerRep*amountOfReps)
+      newCalories = snap.val()[userID].calories - currentCaloriesPerRep*amountOfReps
+
+      database.ref("users/"+userID).update(
+      {
+        calories: newCalories
+      })
+    })
+
+
   });
 
 });
