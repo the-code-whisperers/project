0520fbdf57455b9678e65006dad58c79512d2d12
coreankim@gmail.com <coreankim@gmail.com>
2017-09-09 11:13:48 -0400

    This is ex

--- assets/javascript/exerciseJS.js
+++ assets/javascript/exerciseJS.js
@@ -1,36 +1,43 @@
 $(document).ready(function() {
 
   var populateTodayExercise = function() {
-    for (i = 0; i < phaseOne.dayOne.length; i++) {
-      var exerciseDiv = $("<button>");
-      exerciseDiv.attr("class", "btn btn-primary");
-      exerciseDiv.attr("id", phaseOne.dayOne[i][0]);
-      exerciseDiv.html(phaseOne.dayOne[i][0]); 
-      $("#exercises").append(exerciseDiv);
+    for (var x in phase1.day1) {
+        var exerciseDiv = $("<button>");
+        exerciseDiv.attr("class", "btn btn-primary");
+        exerciseDiv.attr("id", x);
+        exerciseDiv.html(x); 
+        $("#exercises").append(exerciseDiv);
     };
   };
-
-  $("#exercises").on("click", function getExerciseInfo(event) {
-    if (event.target.className=="btn btn-primary") {
-      var selectedExercise = event.target.id;
-            
-    };
-  });
-
+    
   populateTodayExercise();
 
-  //My input needs to be user personal info: height, weight, 
-  //Come up with workout plan per day, based on 
-  //
+  $("#exercises").on("click", function getExerciseInfo(event) {
+    if (event.target.className === "btn btn-primary") {
+      $("#exercise-progress").empty();
+      var selectedExercise = event.target.id;
+            
+      var numSets = phase1.day1[selectedExercise].sets;
+      var numReps = phase1.day1[selectedExercise].reps;
+      for (var i = 0; i < numSets; i++) {
+        $("#exercise-progress").append("Set "+(i+1)+" ");
+        $("#exercise-progress").append("Reps: <span id='rep-counter-"+(i+1)+"'></span><br>");
+        $("#exercise-progress").append("<input type='range' min='0' max='"+numReps+"' value='0' class='slider' id='"+(i+1)+"'>");
+        $("#exercise-progress").append("<label class='checkbox-inline'><input type='checkbox' value='' class='checkbox' id='checkbox-"+(i+1)+"'>Completed?</label><br><br>");     
+      };
+    };
+  });
+  
+  //Create checkbox for whether or not set was completed. True or false?
 
-  // $("#submit").on("click", function getExercise() {
-  //   var newExercise = $("#search-input").val().trim();
-  //   $.ajax({   
-  //     url: queryURL,
-  //     method: "GET",
-  //   }).done(function(response) {
-  //     console.log("I'm working!");
-  //     console.log(response);
-  //   })
-  // });
+  $(document).on("input", ".slider", function(event) {
+    var repCounterID = event.target.id;
+    $("#rep-counter-"+repCounterID).html($(this).val());
+  });
+
+  $(document).on("click", ".checkbox", function(event) {
+    var checkboxID = event.target.id;
+    console.log(checkboxID);
+  });
+
 });
---
+++ assets/javascript/phase1.js
@@ -0,0 +1,124 @@
+var phase1 = { 
+
+	day1: {
+		"Squats": {
+			sets: 5,
+			reps: 10,
+			muscles: "glutes, quadriceps, hamstrings, calves",
+			instructions: "",
+		},
+
+		"Bent Over Rows": {
+			sets: 5,
+			reps: 10,
+		},
+
+		"Overhead Press": {
+			sets: 5,
+			reps: 10,
+		},
+
+		"External Rotations": {
+			sets: 5,
+			reps: 10,
+		},
+
+		"Seated Calf Raises": {
+			sets: 5,
+			reps: 10,
+		},
+	},
+
+	day2: {
+		"Deadlift": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"Kneeling Landmine Press": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"Alternating Arnold Press": {
+			sets: 5,
+			reps: 10,
+		},
+
+		"Alternating Front Lunge": {
+			sets: 3,
+			reps: 15,
+		},
+
+		"Pullovers": {
+			sets: 3,
+			reps: 15,
+		},
+
+		"Weighted Crunches (feet on wall)": {
+			sets: 3,
+			reps: 15,
+		},
+
+		"Side Planks": {
+			timedSets: 3,
+			time: 30,
+		},
+
+		"Cardio": {
+			activity: "Treadmill",
+			time: 30,
+		},
+	},
+
+	day3: {
+		"Front Squat": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"T-Bar Row": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"Dips": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"Upright Row": {
+			sets: 5,
+			reps: 15,
+		},
+
+		"Glute Bridges (weighted)": {
+			sets: 3,
+			reps: 10,
+		},
+
+		"Standing Calf Raises": {
+			sets: 3,
+			reps: 15,
+		},
+
+		"Russian Twists": {
+			timedSets: 3,
+			time: 30,
+		},
+
+		"Cardio": {
+			activity: "Treadmill",
+			time: 30,
+		},
+	},
+}
+
+
+	// Note: Weeks 1-3. Comprehensive workout.
+	// In phase 1, try to decrease your rest times as each week passes. Example: Let’s say you start out
+	// with 90 seconds rest between sets on the 1st week. Try to hit 60 seconds rest time between sets on the
+	// 2nd week, and 30 seconds rest time between sets on the 3rd week. If you want more of an advanced
+	// phase 1 (home edition), try to circuit train through the first four exercises back to back, with only a 90
+	// second rest before starting the next round. After completing the 5 rounds of the first 4 exercises, you will
+	// then move onto the other exercises to finish up.
